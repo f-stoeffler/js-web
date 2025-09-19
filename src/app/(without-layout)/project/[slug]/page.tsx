6578b209e057f34fff07f31830a335b0159852a0
flottschi <florian.stoeffler@edu.htl-villach.at>
2025-09-20 00:44:28 +0200

    furz

--- src/app/(without-layout)/project/[slug]/page.tsx
+++ src/app/(without-layout)/project/[slug]/page.tsx
@@ -1,37 +1,97 @@
-"use server"
-import FeaturedSlide from "@/comps/FeaturedSlide"
-import Footer from "@/comps/Footer"
-import Header from "@/comps/Header"
-import ProjectImagesSlide from "@/comps/ProjectImagesSlide"
-import { getFrontPage } from "@/repo/frontPage"
-import { getProject } from "@/repo/projects"
-import Image from "next/image"
+"use server";
+import FeaturedSlide from "@/comps/FeaturedSlide";
+import Footer from "@/comps/Footer";
+import Header from "@/comps/Header";
+import ProjectImagesSlide from "@/comps/ProjectImagesSlide";
+import { getFrontPage } from "@/repo/frontPage";
+import { getProject } from "@/repo/projects";
+import Image from "next/image";
 import "swiper/css";
-import 'swiper/css/pagination';
+import "swiper/css/pagination";
 
 export default async function ProjectPage({
   params,
 }: {
-  params: Promise<{ slug: string }>
+  params: Promise<{ slug: string }>;
 }) {
-  const { slug } = await params
-    const project = await getProject(slug);
-    console.log(project)
+  const { slug } = await params;
+  const project = await getProject(slug);
+  console.log(project);
 
   return (
     <div className="h-svh flex flex-col">
       <Header />
-      <div className="flex-grow grid grid-cols-12 items-center">
+      <div className="flex-grow grid grid-cols-12 items-center overflow-hidden">
         <div className="col-span-5 pl-8">
-          <div><ProjectImagesSlide imgs={project?.images} /></div>
-                        
+          <div>
+            <ProjectImagesSlide imgs={project?.images} />
+          </div>
         </div>
-        <div className="col-span-7 pl-8 overflow-x-scroll h-96">
-          <h1 className="text-3xl font-bold mb-2.5">{project?.title}</h1>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia harum iste, eos nemo quas sit! Incidunt hic, aspernatur fugiat maiores accusamus totam odit dolore iusto doloremque et magnam iste sit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat, quaerat! Quod velit nihil fugiat assumenda repellendus consectetur vitae veritatis debitis est! Eaque dolorem accusamus magni illo deserunt beatae, praesentium quis? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem modi maxime reprehenderit molestiae ea eveniet, minus unde porro numquam esse consectetur, sint totam veritatis odio nulla alias nihil dolorum animi? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla quidem incidunt quos molestiae optio eum excepturi dolorum non voluptate saepe suscipit libero officiis, aperiam, corporis ratione aliquam! Praesentium, explicabo maxime?</p>
+        <div className="col-span-7 pl-8 overflow-y-scroll max-h-svh">
+          <div className="my-30">
+            <h1 className="text-3xl font-bold mb-2.5">{project?.title}</h1>
+            <p className="text-2xl">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia
+              harum iste, eos nemo quas sit! Incidunt hic, aspernatur fugiat
+              maiores accusamus totam odit dolore iusto doloremque et magnam
+              iste sit? Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Placeat, quaerat! Quod velit nihil fugiat assumenda
+              repellendus consectetur vitae veritatis debitis est! Eaque dolorem
+              accusamus magni illo deserunt beatae, praesentium quis? Lorem
+              ipsum dolor sit amet, consectetur adipisicing elit. Quidem modi
+              maxime reprehenderit molestiae ea eveniet, minus unde porro
+              numquam esse consectetur, sint totam veritatis odio nulla alias
+              nihil dolorum animi? Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Nulla quidem incidunt quos molestiae optio eum
+              excepturi dolorum non voluptate saepe suscipit libero officiis,
+              aperiam, corporis ratione aliquam! Praesentium, explicabo maxime?
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia
+              harum iste, eos nemo quas sit! Incidunt hic, aspernatur fugiat
+              maiores accusamus totam odit dolore iusto doloremque et magnam
+              iste sit? Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Placeat, quaerat! Quod velit nihil fugiat assumenda
+              repellendus consectetur vitae veritatis debitis est! Eaque dolorem
+              accusamus magni illo deserunt beatae, praesentium quis? Lorem
+              ipsum dolor sit amet, consectetur adipisicing elit. Quidem modi
+              maxime reprehenderit molestiae ea eveniet, minus unde porro
+              numquam esse consectetur, sint totam veritatis odio nulla alias
+              nihil dolorum animi? Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Nulla quidem incidunt quos molestiae optio eum
+              excepturi dolorum non voluptate saepe suscipit libero officiis,
+              aperiam, corporis ratione aliquam! Praesentium, explicabo maxime?
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia
+              harum iste, eos nemo quas sit! Incidunt hic, aspernatur fugiat
+              maiores accusamus totam odit dolore iusto doloremque et magnam
+              iste sit? Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Placeat, quaerat! Quod velit nihil fugiat assumenda
+              repellendus consectetur vitae veritatis debitis est! Eaque dolorem
+              accusamus magni illo deserunt beatae, praesentium quis? Lorem
+              ipsum dolor sit amet, consectetur adipisicing elit. Quidem modi
+              maxime reprehenderit molestiae ea eveniet, minus unde porro
+              numquam esse consectetur, sint totam veritatis odio nulla alias
+              nihil dolorum animi? Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Nulla quidem incidunt quos molestiae optio eum
+              excepturi dolorum non voluptate saepe suscipit libero officiis,
+              aperiam, corporis ratione aliquam! Praesentium, explicabo maxime?
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia
+              harum iste, eos nemo quas sit! Incidunt hic, aspernatur fugiat
+              maiores accusamus totam odit dolore iusto doloremque et magnam
+              iste sit? Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Placeat, quaerat! Quod velit nihil fugiat assumenda
+              repellendus consectetur vitae veritatis debitis est! Eaque dolorem
+              accusamus magni illo deserunt beatae, praesentium quis? Lorem
+              ipsum dolor sit amet, consectetur adipisicing elit. Quidem modi
+              maxime reprehenderit molestiae ea eveniet, minus unde porro
+              numquam esse consectetur, sint totam veritatis odio nulla alias
+              nihil dolorum animi? Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Nulla quidem incidunt quos molestiae optio eum
+              excepturi dolorum non voluptate saepe suscipit libero officiis,
+              aperiam, corporis ratione aliquam! Praesentium, explicabo maxime?
+            </p>
+          </div>
         </div>
       </div>
       <Footer />
     </div>
-  )
+  );
 }
