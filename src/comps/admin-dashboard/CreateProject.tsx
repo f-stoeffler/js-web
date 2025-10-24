4081e1d9388551fb83934d7c4560505e9bc785b7
flottschi <florian.stoeffler@edu.htl-villach.at>
2025-10-24 08:40:53 +0200

    german support for slugify

--- src/comps/admin-dashboard/CreateProject.tsx
+++ src/comps/admin-dashboard/CreateProject.tsx
@@ -27,6 +27,12 @@ export default function CreateProjectComp() {
     str = str.replace(/^\s+|\s+$/g, ""); // trim leading/trailing white space
     str = str.toLowerCase(); // convert string to lowercase
     str = str
+      .replace(/ä/g, "a")
+      .replace(/ö/g, "o")
+      .replace(/ü/g, "u")
+      .replace(/ß/g, "ss");
+    str = str
+      .replace(/[^a-z0-9 -]/g, "") // remove any non-alphanumeric characters
       .replace(/[^a-z0-9 -]/g, "") // remove any non-alphanumeric characters
       .replace(/\s+/g, "-") // replace spaces with hyphens
       .replace(/-+/g, "-"); // remove consecutive hyphens
